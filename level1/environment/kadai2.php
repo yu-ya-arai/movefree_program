f0ab60e124f7f2507564b114b277a0484fb70055
新井　雄也 <yunana506@gmail.com>
2025-10-21 00:13:35 +0900

    Add kadai2.php

---
+++ level1/environment/kadai2.php
@@ -0,0 +1,97 @@
+{
+	"folders": [
+		{
+			"name": "HTML研修",
+			"path": "../../../OneDrive/デスクトップ/HTML研修"
+		},
+		{
+			"name": "page1",
+			"path": "../../../OneDrive/デスクトップ/test/src/page1"
+		},
+		{
+			"name": "challenge1",
+			"path": "../../../OneDrive/デスクトップ/challenge1"
+		},
+		{
+			"name": "folder",
+			"path": "../../../OneDrive/デスクトップ/folder"
+		},
+		{
+			"name": "challenge1_新井雄也",
+			"path": "../../../OneDrive/デスクトップ/challenge1_新井雄也"
+		},
+		{
+			"name": "challenge2_新井雄也",
+			"path": "../../../OneDrive/デスクトップ/challenge2_新井雄也"
+		},
+		{
+			"name": "新しいフォルダー",
+			"path": "../../../OneDrive/デスクトップ/新しいフォルダー"
+		},
+		{
+			"name": "challenge3_新井雄也",
+			"path": "../../../OneDrive/デスクトップ/challenge3_新井雄也"
+		},
+		{
+			"name": "05.20研修",
+			"path": "../../../OneDrive/デスクトップ/05.20研修"
+		},
+		{
+			"name": "まとめ課題１_新井雄也",
+			"path": "../../../OneDrive/デスクトップ/まとめ課題１_新井雄也"
+		},
+		{
+			"name": "まとめ課題２_新井雄也",
+			"path": "../../../OneDrive/デスクトップ/まとめ課題２_新井雄也"
+		},
+		{
+			"path": "../../../OneDrive/デスクトップ/css"
+		},
+		{
+			"path": "../../../OneDrive/デスクトップ/challenge4"
+		},
+		{
+			"path": "../../../OneDrive/デスクトップ/challenge5_新井雄也"
+		},
+		{
+			"path": "../../../OneDrive/デスクトップ/練習"
+		},
+		{
+			"path": "../../../OneDrive/デスクトップ/html.css"
+		},
+		{
+			"path": "../../../OneDrive/デスクトップ/challenge6_新井雄也"
+		},
+		{
+			"path": "../../../OneDrive/デスクトップ/練習６・１０"
+		},
+		{
+			"path": "../../../OneDrive/デスクトップ/challenge7_新井雄也"
+		},
+		{
+			"path": "../../../OneDrive/デスクトップ/6月24練習"
+		},
+		{
+			"path": "../../../OneDrive/デスクトップ/challenge8_新井雄也"
+		},
+		{
+			"path": "../../../OneDrive/デスクトップ/challenge9_新井雄也"
+		},
+		{
+			"path": "../../../OneDrive/デスクトップ/challenge11_answer.html"
+		},
+		{
+			"path": "../../../OneDrive/デスクトップ/研修"
+		},
+		{
+			"path": "../../../OneDrive/デスクトップ/研修復習"
+		},
+		{
+			"path": "../../../OneDrive/デスクトップ/challenge11_新井雄也.html"
+		},
+		{
+			"path": "../../../OneDrive/デスクトップ/p101_challenge12"
+		}
+	],
+	"settings": {}
+}
