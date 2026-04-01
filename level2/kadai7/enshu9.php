ab8d899d49f03fd5af207e880e8bc111f772f12a
新井　雄也 <yunana506@gmail.com>
2026-04-01 14:34:13 +0900

    enshu9 修正

--- level2/kadai7/enshu9.php
+++ level2/kadai7/enshu9.php
@@ -2,18 +2,16 @@
 
 $dir = new DirectoryIterator('./');
 
-
 foreach ($dir as $file) {
 
-   
     if ($file->isFile()) {
 
-        
         $name = $file->getFilename();
-        $size = $file->getSize(); 
+        $size = $file->getSize();
         $atime = date('Y/m/d H:i:s', $file->getATime());
         $mtime = date('Y/m/d H:i:s', $file->getMTime());
 
-        print $name . ' | ATime=' . $atime . ' | MTime=' . $mtime . "\n";
+        echo "{$name} | Size={$size}B | ATime={$atime} | MTime={$mtime}\n";
     }
+
 }
